348d3565777bd76a8d55e3eb08f993767a370879
Kalkidanka <122893889+Kalkidanka@users.noreply.github.com>
2025-04-10 16:42:02 +0300

    pages

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Carousel from "./Components/Carousel/Carousel";
+import Category from "./Components/Category/Category";
+import Product from "./Components/Product/Product";
 
 function App() {
   return (
     <div>
       <Header />
       <Carousel />
+      <Category />
+      <Product />
     </div>
   );
 }
---
+++ amazon-clone/src/Components/Category/categoryFullInfos.js
@@ -0,0 +1,26 @@
+export const categoryInfos = [
+  {
+    title: "Electronics",
+    name: "electronics",
+    imgLink:
+      "https://m.media-amazon.com/images/I/81wmp2b-ZAL._AC_UY327_FMwebp_QL65_.jpg",
+  },
+  {
+    title: "Discover fashion trends",
+    name: "women's clothing",
+    imgLink:
+      "https://m.media-amazon.com/images/I/71hzkgODbHL._AC_UL480_FMwebp_QL65_.jpg",
+  },
+  {
+    title: "Men's Clothing",
+    name: "men's clothing",
+    imgLink:
+      "https://m.media-amazon.com/images/I/81eg0LnoPNL._AC_UL480_FMwebp_QL65_.jpg",
+  },
+  {
+    title: "Jewelery",
+    name: "jewelery",
+    imgLink:
+      "https://m.media-amazon.com/images/I/71qlbLY8jNL._AC_UL480_FMwebp_QL65_.jpg",
+  },
+];
